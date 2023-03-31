17522560e8b1e3226466e0a34fba877fd17c775f
superstinker420 <100252287+superstinker420@users.noreply.github.com>
2023-03-31 18:37:16 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+\const timelineItems = document.querySelectorAll('.timeline-item');
 
+function isInViewPort(element) {
+  const rect = element.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+function showVisibleItems() {
+  timelineItems.forEach(item => {
+    if (isInViewPort(item)) {
+      item.classList.add('show');
+    } else {
+      item.classList.remove('show');
+    }
+  });
+}
+
+window.addEventListener('load', showVisibleItems);
+window.addEventListener('resize', showVisibleItems);
+window.addEventListener('scroll', showVisibleItems);
